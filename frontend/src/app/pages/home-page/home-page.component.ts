053773f8aab61fbd0821b75e4e4ba21c21ad21d4
Jose <jrmar0805@gmail.com>
2026-08-07 11:30:13 +0200

    feat: implement load more functionality for article list with pagination

--- frontend/src/app/pages/home-page/home-page.component.ts
+++ frontend/src/app/pages/home-page/home-page.component.ts
@@ -1,10 +1,5 @@
 import { DatePipe } from '@angular/common';
-import {
-  Component,
-  inject,
-  signal,
-  type OnInit
-} from '@angular/core';
+import { Component, computed, inject, signal, type OnInit } from '@angular/core';
 
 import { ArticleApiService } from '../../articles/article-api.service';
 import { ArticleSummary } from '../../articles/article-summary';
@@ -17,43 +12,77 @@ import { RouterLink } from '@angular/router';
   styleUrl: './home-page.component.scss',
 })
 export class HomePage implements OnInit {
+  private readonly pageSize = 5;
+
+  protected readonly currentPage = signal(0);
+
+  protected readonly totalPages = signal(0);
+
+  protected readonly isLoadingMore = signal(false);
+
+  protected readonly loadMoreError = signal(false);
+
+  protected readonly hasMoreArticles = computed(() => this.currentPage() + 1 < this.totalPages());
   private readonly articleApiService = inject(ArticleApiService);
 
-  protected readonly featuredArticle =
-    signal<ArticleSummary | null>(null);
+  protected readonly featuredArticle = signal<ArticleSummary | null>(null);
 
-  protected readonly articles =
-    signal<ArticleSummary[]>([]);
+  protected readonly articles = signal<ArticleSummary[]>([]);
 
   protected readonly isLoading = signal(true);
   protected readonly loadError = signal(false);
 
   ngOnInit(): void {
-    this.articleApiService
-      .getPublishedArticles(0, 10)
-      .subscribe({
-        next: response => {
-          const [
-            featuredArticle,
-            ...remainingArticles
-          ] = response.content;
+    this.articleApiService.getPublishedArticles(0, this.pageSize).subscribe({
+      next: (response) => {
+        const [featuredArticle, ...remainingArticles] = response.content;
 
-          this.featuredArticle.set(
-            featuredArticle ?? null
-          );
+        this.featuredArticle.set(featuredArticle ?? null);
 
-          this.articles.set(remainingArticles);
-          this.isLoading.set(false);
-        },
-        error: error => {
-          console.error(
-            'Could not load published articles',
-            error
-          );
+        this.articles.set(remainingArticles);
 
-          this.loadError.set(true);
-          this.isLoading.set(false);
-        }
-      });
+        this.currentPage.set(response.page);
+
+        this.totalPages.set(response.totalPages);
+
+        this.isLoading.set(false);
+      },
+      error: (error) => {
+        console.error('Could not load published articles', error);
+
+        this.loadError.set(true);
+        this.isLoading.set(false);
+      },
+    });
+  }
+
+  protected loadMoreArticles(): void {
+    if (this.isLoadingMore() || !this.hasMoreArticles()) {
+      return;
+    }
+
+    const nextPage = this.currentPage() + 1;
+
+    this.isLoadingMore.set(true);
+    this.loadMoreError.set(false);
+
+    this.articleApiService.getPublishedArticles(nextPage, this.pageSize).subscribe({
+      next: (response) => {
+        this.articles.update((currentArticles) => [...currentArticles, ...response.content]);
+
+        this.currentPage.set(response.page);
+
+        this.totalPages.set(response.totalPages);
+
+        this.isLoadingMore.set(false);
+      },
+
+      error: (error) => {
+        console.error('Could not load more published articles', error);
+
+        this.loadMoreError.set(true);
+        this.isLoadingMore.set(false);
+      },
+    });
   }
 }
